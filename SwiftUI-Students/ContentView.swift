b0fda0673ecce28240cc70779f0dafeff67e8d16
Christoph Krassnigg <35232234+Gebes@users.noreply.github.com>
2020-06-26 20:11:41 +0200

    Initial Commit

---
+++ SwiftUI-Students/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Students
+//
+//  Created by Christoph Krassnigg on 26.06.20.
+//  Copyright © 2020 Christoph Krassnigg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
